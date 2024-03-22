955d91fee6a02a8e1ae14d27842738d93e739ddc
Ing.w <william.esteban@campusucc.edu.com>
2024-03-22 15:07:51 -0500

    DatosSocio

---
+++ Test/TestDatosSocioEconomicos.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SiianTest.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiianTest.Test
+{
+    [TestClass]
+    public class TestDatosSocioEconomicos
+    {
+
+        [TestMethod]
+        public void Get()
+        {
+            Credenciales credenciales = new Credenciales();
+            ApiUrls apiUrls = new ApiUrls();
+
+            using (HttpClient cliente = new HttpClient())
+            {
+                using (HttpRequestMessage httpmensaje = new HttpRequestMessage())
+                {
+                    httpmensaje.RequestUri = new Uri(apiUrls.GetUrl("DatosSocioEconomicos"));
+                    httpmensaje.Method = HttpMethod.Get;
+                    httpmensaje.Headers.Add("Accept", "application/json");
+
+                    httpmensaje.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", credenciales.Token);
+
+                    Task<HttpResponseMessage> httpResponse = cliente.SendAsync(httpmensaje);
+
+                    using (HttpResponseMessage mensaje = httpResponse.Result)
+                    {
+                        Console.WriteLine(mensaje.ToString());
+
+                        HttpStatusCode statusCode = mensaje.StatusCode;
+
+                        HttpContent respuestaContenido = mensaje.Content;
+                        Task<string> respuestaData = respuestaContenido.ReadAsStringAsync();
+                        string data = respuestaData.Result;
+
+                        RestResponse restResponse = new RestResponse((int)statusCode, respuestaData.Result);
+                        Console.WriteLine(restResponse);
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void PostParametros()
+        {
+            Credenciales credenciales = new Credenciales();
+            ApiUrls apiUrls = new ApiUrls();
+
+            using (HttpClient cliente = new HttpClient())
+            {
+                using (HttpRequestMessage httpmensaje = new HttpRequestMessage())
+                {
+                    httpmensaje.RequestUri = new Uri(apiUrls.GetUrl("DatosSocioEconomicos"));
+                    httpmensaje.Method = HttpMethod.Post;
+                    httpmensaje.Headers.Add("Accept", "application/json");
+
+                    httpmensaje.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", credenciales.Token);
+
+                    Task<HttpResponseMessage> httpResponse = cliente.SendAsync(httpmensaje);
+
+                    using (HttpResponseMessage mensaje = httpResponse.Result)
+                    {
+                        Console.WriteLine(mensaje.ToString());
+
+                        HttpStatusCode statusCode = mensaje.StatusCode;
+
+                        HttpContent respuestaContenido = mensaje.Content;
+                        Task<string> respuestaData = respuestaContenido.ReadAsStringAsync();
+                        string data = respuestaData.Result;
+
+                        RestResponse restResponse = new RestResponse((int)statusCode, respuestaData.Result);
+                        Console.WriteLine(restResponse);
+                    }
+                }
+            }
+        }
+    }
+}
