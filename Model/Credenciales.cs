5646a6d7378cf1ff415dbbf1dc96035958d47424
srwesteban <sr.w@hotmail.com>
2024-03-22 13:51:51 -0500

    GitPull

--- Model/Credenciales.cs
+++ Model/Credenciales.cs
@@ -8,10 +8,10 @@ namespace SiianTest.Model
 {
     public class Credenciales
     {
+        public string Token { get; } = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkFETUlOSVNUUkFET1IiLCJuYW1laWQiOiI4MzdmNDk4NS01NmM0LTQ5OWItYWE2Ni05MmVjZTQ0NzQzOTMiLCJuYmYiOjE3MTExMzI4NzEsImV4cCI6MTcxMTEzNDY3MSwiaWF0IjoxNzExMTMyODcxLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdCJ9.NdMpu0zQVQuysyZbNkXNIJPOh9G4LxAKlsVmPHsYqCI";
+
         public string UserName { get; } = "ADMINISTRADOR";
         public string Password { get; } = "Sii@n123*";
-
-        public string Token { get; } = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkFETUlOSVNUUkFET1IiLCJuYW1laWQiOiI4MzdmNDk4NS01NmM0LTQ5OWItYWE2Ni05MmVjZTQ0NzQzOTMiLCJuYmYiOjE3MTExMjE1MTYsImV4cCI6MTcxMTEyMzMxNiwiaWF0IjoxNzExMTIxNTE2LCJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdCJ9.i6g-wJo8m_mW8b5LZOGETJLhgxhled7E7re0Zh3vGgY";
 
         public string LoginCredentials
         {
