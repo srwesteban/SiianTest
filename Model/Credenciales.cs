595a57b4ba1409e57a7e5b1863672599e1ea2938
srwesteban <sr.w@hotmail.com>
2024-03-23 11:58:39 -0500

    Token

--- Model/Credenciales.cs
+++ Model/Credenciales.cs
@@ -9,9 +9,6 @@ namespace SiianTest.Model
 {
     public class Credenciales
     {
-        public string Token { get; }
-
-    = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IkFETUlOSVNUUkFET1IiLCJuYW1laWQiOiI4MzdmNDk4NS01NmM0LTQ5OWItYWE2Ni05MmVjZTQ0NzQzOTMiLCJuYmYiOjE3MTEyMDY0ODMsImV4cCI6MTcxMTIwODI4MywiaWF0IjoxNzExMjA2NDgzLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdCJ9.w7DqtsjK7zdewyTJm4ZOLFbZTwqee5kstn4q8WvNH7I";
         public string UserName { get; } = "ADMINISTRADOR";
         public string Password { get; } = "Sii@n123*";
 
